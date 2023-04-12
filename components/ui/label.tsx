3d393d74b9dfeb29fd910b90f877d693e0a91914
qudus <qudus@ecscorpresources.com>
2023-04-12 14:10:29 +0100

    feat: multistep form logic

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { cn } from "../../utils";
+import { cn } from "../../lib/utils";
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
---
+++ hooks/useMultistepForm.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const useMultistepForm = (step: number) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState<number>(0);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  const nextStep = () => {
+    if (currentStepIndex < step - 1) {
+      setCurrentStepIndex((i) => i + 1);
+    }
+    if (currentStepIndex === 3) {
+      setIsSuccess(true);
+    }
+  };
+
+  const previousStep = () => {
+    if (currentStepIndex > 0) {
+      setCurrentStepIndex((i) => i - 1);
+    }
+  };
+
+  const gotoForm = (index: number) => {
+    setCurrentStepIndex(index);
+  };
+
+  const isFirstStep = currentStepIndex === 0;
+  const isLastStep = currentStepIndex === step - 1;
+
+  return {
+    nextStep,
+    previousStep,
+    gotoForm,
+    isFirstStep,
+    isLastStep,
+    isSuccess,
+  };
+};
